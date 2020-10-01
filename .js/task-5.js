c73068a7630fa9304f8e997499163d244f6cd9e9
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-01 13:28:23 +0300

    задача 5

---
+++ .js/task-5.js
@@ -0,0 +1,44 @@
+// function checkForSpam(str) {
+//     if (str.includes('sale')) {
+//         return true
+//     } else {
+//         return false
+//     }
+
+//     // if (str.includes('[SPAM]')) {
+//     //     return false
+//     // } else {
+//     //     return true
+//     // }
+
+// }
+
+
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?'));
+
+
+
+// ? WHY IS NOT A FUNCTION??
+// const hotel = {
+//     name: 'Resort Hotel',
+//     stars: 5,
+//     capacity: 100,
+
+//     greetInES6() {
+//         console.log('Welcome!');
+//     },
+// };
+
+// hotel.greetInEs6(); // Welcome!
+
+
+
+
+
+
