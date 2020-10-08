22f9560da8d3911503536232f7b7d72cd75ed57a
Alina Leonova <alinaa.leonova@gmail.com>
2020-10-08 09:02:22 +0300

    task 5

--- .js/task-5.js
+++ .js/task-5.js
@@ -1,44 +1,19 @@
-// function checkForSpam(str) {
-//     if (str.includes('sale')) {
-//         return true
-//     } else {
-//         return false
-//     }
+function checkForSpam(str) {
 
-//     // if (str.includes('[SPAM]')) {
-//     //     return false
-//     // } else {
-//     //     return true
-//     // }
-
-// }
-
-
-// console.log(checkForSpam('Latest technology news')); // false
-
-// console.log(checkForSpam('JavaScript weekly newsletter')); // false
-
-// console.log(checkForSpam('Get best sale offers now!')); // true
-
-// console.log(checkForSpam('[SPAM] How to earn fast money?'));
+    return str.toLowerCase().includes('sale') || str.toLowerCase().includes('spam')
 
 
 
-// ? WHY IS NOT A FUNCTION??
-// const hotel = {
-//     name: 'Resort Hotel',
-//     stars: 5,
-//     capacity: 100,
-
-//     greetInES6() {
-//         console.log('Welcome!');
-//     },
-// };
-
-// hotel.greetInEs6(); // Welcome!
+}
 
 
+console.log(checkForSpam('Latest technology news')); // false
 
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
 
 
 
